5dc4c161640e10e97838275439042e7292a202f1
Hotturtle1 <ahmadmustafa287@yahoo.com>
2023-06-24 16:14:57 +0300

    pp

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
      images :{
           domains : ["images.unsplash.com","c4.wallpaperflare.com",'avatars.githubusercontent.com','cdn.discordapp.com','upload.wikimedia.org']
 
+     } ,
+     env : {
+          NEXTAUTH_URL: process.env.NEXTAUTH_URL
      }
 }
 
